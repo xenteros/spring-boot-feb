b1474a88ac7af3c79322cb0a84fdc11fa03f8f6d
xenteros <adam@gurgul.com.pl>
2018-02-12 09:05:53 +0100

    Add Hello World example

---
+++ src/main/java/com/github/xenteros/Application.java
@@ -0,0 +1,23 @@
+package com.github.xenteros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+
+}
